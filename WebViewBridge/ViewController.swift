965bd3f87740049cb7a35cd7de75499e982d25bb
Kurt Prenger <kurt.prenger@sourcebits.com>
2018-09-12 15:06:45 -0400

    Feat: Implementing JS bridge and integration of picture taking from a JS bridge.

--- WebViewBridge/ViewController.swift
+++ WebViewBridge/ViewController.swift
@@ -6,12 +6,17 @@
 //  Copyright © 2018 Sourcebits. All rights reserved.
 //
 
+import MobileCoreServices
 import UIKit
 import WebKit
 
 class ViewController: UIViewController {
     
+    private let takeSelfieMsg = "takeSelfie"
+    private let closeWebViewMsg = "closeWebView"
+    
     private var webView: WKWebView?
+    private var webViewController = UIViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,33 +25,63 @@ class ViewController: UIViewController {
         
         guard let webView = webView, let resourceUrl = Bundle.main.resourceURL else { return }
         
-        webView.configuration.userContentController.add(self, name: "takeSelfie")
-        webView.configuration.userContentController.add(self, name: "closeWebView")
+        webView.configuration.userContentController.add(self, name: takeSelfieMsg)
+        webView.configuration.userContentController.add(self, name: closeWebViewMsg)
         
         let bundleUrl = resourceUrl.absoluteURL
         let html = bundleUrl.appendingPathComponent("webpage.html")
         
         webView.loadFileURL(html, allowingReadAccessTo: bundleUrl)
+        webViewController.view.addSubview(webView)
     }
 
     // MARK: - Button touches
     @IBAction func openWebview(_ sender: UIButton) {
-        guard let webView = webView else { return }
-        view.addSubview(webView)
+        present(webViewController, animated: true, completion: nil)
     }
 }
 
 extension ViewController: WKScriptMessageHandler {
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        
+        switch message.name {
+        case closeWebViewMsg:
+            closeWebView()
+        case takeSelfieMsg:
+            takeSelfie()
+        default:
+            closeWebView()
+        }
     }
     
     func takeSelfie() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera),
+            UIImagePickerController.isCameraDeviceAvailable(.front),
+            let types = UIImagePickerController.availableMediaTypes(for: .camera),
+            types.contains(kUTTypeMovie as String) else {
+            
+            print("no camera to record video available")
+            closeWebView()
+            return
+        }
         
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.cameraDevice = .front
+        imagePicker.mediaTypes = [kUTTypeMovie as String]
+        imagePicker.videoMaximumDuration = 10
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        webViewController.present(imagePicker, animated: true, completion: nil)
     }
     
     func closeWebView() {
-        
+        webViewController.dismiss(animated: true, completion: nil)
     }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("got an image")
+    }
+}
