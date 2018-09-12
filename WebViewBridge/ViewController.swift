f4e89b1920aba0f3ff9de97cfa50c1ffa2876e9a
Kurt Prenger <kurt.prenger@sourcebits.com>
2018-09-12 14:07:16 -0400

    Feat: Added basic webpage and displaying it via button click.

--- WebViewBridge/ViewController.swift
+++ WebViewBridge/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    private var webView: WKWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView(frame: view.frame)
+        
+        guard let webView = webView, let resourceUrl = Bundle.main.resourceURL else { return }
+        
+        webView.configuration.userContentController.add(self, name: "takeSelfie")
+        webView.configuration.userContentController.add(self, name: "closeWebView")
+        
+        let bundleUrl = resourceUrl.absoluteURL
+        let html = bundleUrl.appendingPathComponent("webpage.html")
+        
+        webView.loadFileURL(html, allowingReadAccessTo: bundleUrl)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Button touches
+    @IBAction func openWebview(_ sender: UIButton) {
+        guard let webView = webView else { return }
+        view.addSubview(webView)
     }
+}
 
-
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+    }
+    
+    func takeSelfie() {
+        
+    }
+    
+    func closeWebView() {
+        
+    }
 }
 
